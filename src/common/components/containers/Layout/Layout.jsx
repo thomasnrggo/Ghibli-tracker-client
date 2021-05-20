08d8de9b3bbc3e7a45cee21f19476f52c88af8c7
Anthony Gonzalez <thomasnrggo@gmail.com>
2021-05-19 20:52:24 -0500

    homepage grid layout

--- src/common/components/containers/Layout/Layout.jsx
+++ src/common/components/containers/Layout/Layout.jsx
@@ -5,7 +5,6 @@ export default function Layout({ children }) {
   return (
     <>
       <Header className="container" />
-
       {/* TODO: define if we are gointo to use a fluid container */}
       <main className="main container">
         {children}
