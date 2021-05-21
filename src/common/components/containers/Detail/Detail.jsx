8167349b6ea1deadba868092eb60adefff2368f7
Johnnatan Reyes Garcia <tanamix22@gmail.com>
2021-05-21 15:12:21 -0500

    change in scss of detail

--- src/common/components/containers/Detail/Detail.jsx
+++ src/common/components/containers/Detail/Detail.jsx
@@ -4,23 +4,26 @@ import movie from './movie.json';
 
 export default function Detail(props) {
   return (
-    <div className={styles.detailsContainer}>
-      <img
-        className={`${styles.col} ${styles.movieImage}`}
-        src={movie.imageUrl}
-        alt={movie.title}
-      />
-      <div className={`${styles.col} ${styles.movieDetails}`}>
-        <p className={styles.firstItem}>
-          <strong>Title:</strong> {movie.title}
-        </p>
-        <p>
-          <strong>Genres:</strong>{' '}
-          {movie.genres.map((genre) => genre.name).join(', ')}
-        </p>
-        <p>
-          <strong>Description:</strong> {movie.overview}
-        </p>
+    <div className={styles.container}>
+      <h1>{movie.title}</h1>
+      <div className={styles.container__detail}>
+        <img
+          className={`${styles.col} ${styles.movieImage}`}
+          src={movie.imageUrl}
+          alt={movie.title}
+        />
+        <div className={`${styles.col} ${styles.movieDetails}`}>
+          <p className={styles.firstItem}>
+            <strong>Title:</strong> {movie.title}
+          </p>
+          <p>
+            <strong>Genres:</strong>{' '}
+            {movie.genres.map((genre) => genre.name).join(', ')}
+          </p>
+          <p>
+            <strong>Description:</strong> {movie.overview}
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/pages/film/[id].jsx
+++ src/pages/film/[id].jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Layout from '../../common/components/containers/Layout/Layout';
 import Detail from '../../common/components/containers/Detail/Detail';
-import styles from '../../common/components/containers/Detail/Detail.module.scss';
 
-export default function FilmDetail(props) {
+export default function MovieDetail(props) {
   return (
     <>
       <Layout>
-        <h1 className="h1">Tltle</h1>
         <Detail />
       </Layout>
     </>
