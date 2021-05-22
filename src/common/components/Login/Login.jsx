719c42020198bbb2e4bf2c8d5a5fd63517e73f18
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-05-22 17:49:42 -0600

    Creating Sign up form layout. Switching between login and signup forms. Fixed modal responsive design

--- src/common/components/Login/Login.jsx
+++ src/common/components/Login/Login.jsx
@@ -13,6 +13,17 @@ export default function Login() {
     email: false,
     password: false,
   });
+  const [signupData, setSignupData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+  const [signupFormValidation, setSignupFormValidation] = useState({
+    username: false,
+    email: false,
+    password: false,
+  });
+  const [currentTab, setCurrentTab] = useState('login');
   const { state, dispatch } = useContext(store);
   const { isOpen } = state;
 
@@ -23,17 +34,20 @@ export default function Login() {
 
     const options = {
       login: () => setLoginData((state) => ({ ...state, ...data })),
-      signup: () => setSignupData(),
+      signup: () => setSignupData((state) => ({ ...state, ...data })),
     };
 
     return options[type]();
   }
 
   function validateForm(type) {
-    const { email, password } = loginData;
-
     const validations = {
       login: {
+        email: /\S+@\S+\.\S+/,
+        password: /(?=.{8,})/,
+      },
+      signup: {
+        username: /(?=.{8,20}$)[a-zA-Z0-9._]/,
         email: /\S+@\S+\.\S+/,
         password: /(?=.{8,})/,
       },
@@ -41,9 +55,21 @@ export default function Login() {
     const options = {
       login: () =>
         setLoginFormValidation((state) => ({
-          email: validations.login.email.test(email.trim().toLowerCase()),
-          password: validations.login.password.test(
-            password.trim().toLowerCase()
+          email: validations.login.email.test(
+            loginData.email.trim().toLowerCase()
+          ),
+          password: validations.login.password.test(loginData.password.trim()),
+        })),
+      signup: () =>
+        setSignupFormValidation((state) => ({
+          username: validations.signup.username.test(
+            signupData.username.trim().toLowerCase()
+          ),
+          email: validations.signup.username.test(
+            signupData.email.trim().toLowerCase()
+          ),
+          password: validations.signup.password.test(
+            signupData.password.trim()
           ),
         })),
     };
@@ -62,67 +88,179 @@ export default function Login() {
       onClose={() => dispatch({ type: 'MODAL_TRIGGER', modal: 'login' })}
       className={styles.login__container}
     >
-      <p className={`${styles.login__title} h3`}>Log in</p>
+      <p className={`${styles.login__title} h3`}>
+        {currentTab === 'login' ? 'Log In' : 'Sign Up'}
+      </p>
 
       <form
         className={styles.login__form}
-        onSubmit={(e) => handleSubmit(e, 'login')}
+        onSubmit={(e) => handleSubmit(e, currentTab)}
       >
-        <div
-          className={`input__container ${
-            loginFormValidation.email ? 'input-success' : 'input-error'
-          }`}
-        >
-          <label htmlFor="email" className="input__label">
-            E-mail
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="input"
-              placeholder="Your email here..."
-              value={loginData.email}
-              onChange={(e) => handleChange(e, 'login')}
-            />
-          </label>
+        {currentTab === 'login' ? (
+          <>
+            <div
+              className={`input__container ${
+                loginFormValidation.email ? 'input-success' : 'input-error'
+              }`}
+            >
+              <label htmlFor="email" className="input__label">
+                E-mail
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="input"
+                  placeholder="Your email here..."
+                  value={loginData.email}
+                  onChange={(e) => handleChange(e, 'login')}
+                />
+              </label>
 
-          <p className="input__helper">
-            {!loginFormValidation.email && 'Enter a valid email.'}
-          </p>
-        </div>
+              <p className="input__helper">
+                {!loginFormValidation.email && 'Enter a valid email.'}
+              </p>
+            </div>
 
-        <div
-          className={`input__container ${
-            loginFormValidation.password ? 'input-success' : 'input-error'
-          }`}
-        >
-          <label htmlFor="password" className="input__label">
-            Password
-            <input
-              type="password"
-              name="password"
-              id="password"
-              className="input"
-              placeholder="Type your password..."
-              value={loginData.password}
-              onChange={(e) => handleChange(e, 'login')}
-            />
-          </label>
+            <div
+              className={`input__container ${
+                loginFormValidation.password ? 'input-success' : 'input-error'
+              }`}
+            >
+              <label htmlFor="password" className="input__label">
+                Password
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="input"
+                  placeholder="Type your password..."
+                  value={loginData.password}
+                  onChange={(e) => handleChange(e, 'login')}
+                />
+              </label>
 
-          <p className="input__helper">
-            {!loginFormValidation.password &&
-              'The password must contain 8 characters.'}
-          </p>
-        </div>
+              <p className="input__helper">
+                {!loginFormValidation.password &&
+                  'The password must contain 8 characters.'}
+              </p>
+            </div>
 
-        <button
-          type="submit"
-          className={`btn btn-primary ${styles.login__btn}`}
-          formNoValidate="formnovalidate"
-        >
-          Enter{' '}
-          <i className={`fas fa-chevron-right ${styles.login__btn_icon}`} />
-        </button>
+            <div className={styles.login__buttons}>
+              <button
+                type="button"
+                className={`btn btn-secondary ${styles.login__btn}`}
+                onClick={() => setCurrentTab('signup')}
+              >
+                Sign Up
+              </button>
+
+              <button
+                type="submit"
+                className={`btn btn-primary ${styles.login__btn}`}
+                formNoValidate="formnovalidate"
+              >
+                Log In{' '}
+                <i
+                  className={`fas fa-chevron-right ${styles.login__btn_icon}`}
+                />
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <div
+              className={`input__container ${
+                signupFormValidation.username ? 'input-success' : 'input-error'
+              }`}
+            >
+              <label htmlFor="username" className="input__label">
+                username
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  className="input"
+                  placeholder="Your email here..."
+                  value={signupData.username}
+                  onChange={(e) => handleChange(e, 'signup')}
+                />
+              </label>
+
+              <p className="input__helper">
+                {!signupFormValidation.email &&
+                  'Username must contain between 8 and 20 characters.'}
+              </p>
+            </div>
+
+            <div
+              className={`input__container ${
+                signupFormValidation.email ? 'input-success' : 'input-error'
+              }`}
+            >
+              <label htmlFor="email" className="input__label">
+                E-mail
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="input"
+                  placeholder="Your email here..."
+                  value={signupData.email}
+                  onChange={(e) => handleChange(e, 'signup')}
+                />
+              </label>
+
+              <p className="input__helper">
+                {!signupFormValidation.email && 'Enter a valid email.'}
+              </p>
+            </div>
+
+            <div
+              className={`input__container ${
+                signupFormValidation.password ? 'input-success' : 'input-error'
+              }`}
+            >
+              <label htmlFor="password" className="input__label">
+                Password
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="input"
+                  placeholder="Type your password..."
+                  value={signupData.password}
+                  onChange={(e) => handleChange(e, 'signup')}
+                />
+              </label>
+
+              <p className="input__helper">
+                {!signupFormValidation.password &&
+                  'The password must contain 8 characters.'}
+              </p>
+            </div>
+
+            <div className={styles.login__buttons}>
+              <button
+                type="submit"
+                className={`btn btn-secondary ${styles.login__btn}`}
+                onClick={() => setCurrentTab('login')}
+              >
+                Log In
+              </button>
+
+              <button
+                type="submit"
+                className={`btn btn-primary ${styles.login__btn}`}
+                formNoValidate="formnovalidate"
+              >
+                Sign Up{' '}
+                <i
+                  className={`fas fa-chevron-right ${styles.login__btn_icon}`}
+                />
+              </button>
+            </div>
+          </>
+        )}
 
         <hr className="divider" />
 
