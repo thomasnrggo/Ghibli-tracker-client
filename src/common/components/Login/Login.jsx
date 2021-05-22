43e48224421796a4cb8112b96ca63c930be26379
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-05-22 17:19:25 -0600

    Added field validations to login form

--- src/common/components/Login/Login.jsx
+++ src/common/components/Login/Login.jsx
@@ -1,13 +1,60 @@
-import React, { useContext } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useContext, useState } from 'react';
 import Modal from '../Modal/Modal';
 import { store } from '../../context/store';
 
 import styles from './Login.module.scss';
 
 export default function Login() {
+  const [loginData, setLoginData] = useState({
+    email: '',
+    password: '',
+  });
+  const [loginFormValidation, setLoginFormValidation] = useState({
+    email: false,
+    password: false,
+  });
   const { state, dispatch } = useContext(store);
   const { isOpen } = state;
+
+  function handleChange(e, type) {
+    const { name, value } = e.target;
+    const data = {};
+    data[name] = value;
+
+    const options = {
+      login: () => setLoginData((state) => ({ ...state, ...data })),
+      signup: () => setSignupData(),
+    };
+
+    return options[type]();
+  }
+
+  function validateForm(type) {
+    const { email, password } = loginData;
+
+    const validations = {
+      login: {
+        email: /\S+@\S+\.\S+/,
+        password: /(?=.{8,})/,
+      },
+    };
+    const options = {
+      login: () =>
+        setLoginFormValidation((state) => ({
+          email: validations.login.email.test(email.trim().toLowerCase()),
+          password: validations.login.password.test(
+            password.trim().toLowerCase()
+          ),
+        })),
+    };
+
+    return options[type]();
+  }
+
+  function handleSubmit(e, type) {
+    e.preventDefault();
+    validateForm(type);
+  }
 
   return (
     <Modal
@@ -17,8 +64,15 @@ export default function Login() {
     >
       <p className={`${styles.login__title} h3`}>Log in</p>
 
-      <form className={styles.login__form} onSubmit={(e) => e.preventDefault()}>
-        <div className="input__container">
+      <form
+        className={styles.login__form}
+        onSubmit={(e) => handleSubmit(e, 'login')}
+      >
+        <div
+          className={`input__container ${
+            loginFormValidation.email ? 'input-success' : 'input-error'
+          }`}
+        >
           <label htmlFor="email" className="input__label">
             E-mail
             <input
@@ -27,11 +81,21 @@ export default function Login() {
               id="email"
               className="input"
               placeholder="Your email here..."
+              value={loginData.email}
+              onChange={(e) => handleChange(e, 'login')}
             />
           </label>
+
+          <p className="input__helper">
+            {!loginFormValidation.email && 'Enter a valid email.'}
+          </p>
         </div>
 
-        <div className="input__container">
+        <div
+          className={`input__container ${
+            loginFormValidation.password ? 'input-success' : 'input-error'
+          }`}
+        >
           <label htmlFor="password" className="input__label">
             Password
             <input
@@ -40,13 +104,21 @@ export default function Login() {
               id="password"
               className="input"
               placeholder="Type your password..."
+              value={loginData.password}
+              onChange={(e) => handleChange(e, 'login')}
             />
           </label>
+
+          <p className="input__helper">
+            {!loginFormValidation.password &&
+              'The password must contain 8 characters.'}
+          </p>
         </div>
 
         <button
           type="submit"
           className={`btn btn-primary ${styles.login__btn}`}
+          formNoValidate="formnovalidate"
         >
           Enter{' '}
           <i className={`fas fa-chevron-right ${styles.login__btn_icon}`} />
