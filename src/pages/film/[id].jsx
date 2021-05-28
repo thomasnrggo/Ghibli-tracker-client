754b383b8e13ecb79164c406d4913bc468188421
Johnnatan Reyes Garcia <tanamix22@gmail.com>
2021-05-27 20:25:57 -0500

    reviw error in details

--- src/pages/film/[id].jsx
+++ src/pages/film/[id].jsx
@@ -1,19 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Layout from '../../common/components/Layout/Layout';
 import Detail from '../../common/components/Detail/Detail';
-import films from '../../common/data/films.json';
 
 export default function MovieDetail(props) {
-  const [films, setFilms] = useState([]);
-  /* 
-  useEffect(() => {
-    fetch(films)
-      .then((Response) => Response.json())
-      .then((data) => setFilms(data));
-  }, []);
-
-  console.log(films);
- */
   return (
     <>
       <Layout>
