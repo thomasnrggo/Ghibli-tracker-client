5fdbd4189daa6d8ee5fcf199fdd01dfe47e8518d
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-05-31 20:25:35 -0600

    Managing redirects for the profile page whether a user is logged in

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import { signOut } from 'next-auth/client';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { signOut, useSession } from 'next-auth/client';
+import Loader from '../common/components/Loader/Loader';
 import Layout from '../common/components/Layout/Layout';
 
 export default function profile() {
+  const [session, loading] = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session && !loading) router.push('/');
+  }, [session, loading]);
+
   return (
     <Layout>
-      <button
-        className="btn btn-primary"
-        onClick={() => signOut({ callbackUrl: '/' })}
-      >
-        Log out
-      </button>
+      {!session || loading ? (
+        <Loader />
+      ) : (
+        <button
+          className="btn btn-primary"
+          onClick={() => signOut({ callbackUrl: '/' })}
+        >
+          Log out
+        </button>
+      )}
     </Layout>
   );
 }
