e3724c64e0e6d03f4d5a3712481b39e2158a8848
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-06-02 13:37:45 -0600

    Merge solve

--- src/common/components/Header/Header.jsx
+++ src/common/components/Header/Header.jsx
@@ -10,7 +10,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Login from '../Login/Login';
-import Autocomplete from '../autocomplete/Autocomplete'
+import Autocomplete from '../autocomplete/Autocomplete';
 import styles from './Header.module.scss';
 import { getFilms } from '../../utils/services';
 
@@ -18,7 +18,7 @@ export default function Header() {
   const [session, loading] = useSession();
   const { dispatch, state } = useContext(store);
   const { isSearchActive } = state;
-  const [films, setFilms] = useState([])
+  const [films, setFilms] = useState([]);
   const router = useRouter();
 
   let onSearchIconClick = () => {
@@ -43,35 +43,17 @@ export default function Header() {
       dispatch({ type: 'AUTH_TRIGGER' });
 
     getFilms()
-    .then(res => {
-      setFilms(res)
-    })
-    .catch(err => {
-      console.error(err);
-    })
-    
+      .then((res) => {
+        setFilms(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }, [router]);
 
   return (
     <>
       <header className={styles.header}>
-<<<<<<< HEAD
-        <div className={styles.icon}>
-          {router.pathname !== '/' ? (
-            <FontAwesomeIcon
-              icon={faChevronLeft}
-              onClick={() => router.back()}
-            />
-          ) : (
-            <>
-              <FontAwesomeIcon
-                icon={session ? faUser : faSignInAlt}
-                onClick={handleProfile}
-              />
-            </>
-          )}
-        </div>
-=======
         <div className={styles.menu__container}>
           <div className={styles.icon}>
             {router.pathname !== '/' ? (
@@ -80,21 +62,23 @@ export default function Header() {
                 onClick={() => router.back()}
               />
             ) : (
-              <FontAwesomeIcon icon={faUser} onClick={handleProfile} />
+              <FontAwesomeIcon
+                icon={session ? faUser : faSignInAlt}
+                onClick={handleProfile}
+              />
             )}
           </div>
->>>>>>> development
 
-          <img className={styles.logo} src="/SVG/logo.svg" alt="Ghibli tracker" />
+          <img
+            className={styles.logo}
+            src="/SVG/logo.svg"
+            alt="Ghibli tracker"
+          />
 
           <div className={`${styles.icon} ${styles.autocomplete__container}`}>
             {isSearchActive && (
               <div className={styles.search__container}>
-                {films.length >= 1 && (
-                  <Autocomplete
-                    suggestions={films}
-                  />
-                )}
+                {films.length >= 1 && <Autocomplete suggestions={films} />}
               </div>
             )}
             <FontAwesomeIcon
@@ -105,16 +89,10 @@ export default function Header() {
         </div>
         {isSearchActive && (
           <div className={styles.search__onMobile}>
-            {films.length >= 1 && (
-                <Autocomplete
-                  suggestions={films}
-                />
-              )}
+            {films.length >= 1 && <Autocomplete suggestions={films} />}
           </div>
         )}
-        
       </header>
-      
 
       <Login />
     </>
