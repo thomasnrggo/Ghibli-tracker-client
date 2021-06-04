972be9c43ddea8f740319dff2063ef6d29ae0b0a
Johnnatan Reyes Garcia <tanamix22@gmail.com>
2021-06-03 22:47:48 -0500

    change in detail post

--- src/common/components/Detail/Detail.jsx
+++ src/common/components/Detail/Detail.jsx
@@ -5,15 +5,20 @@ import { useRouter } from 'next/router';
 import { getFilmsDetail } from '../../utils/services';
 import Modal from '../Modal/Modal';
 import { store } from '../../context/store';
+import { useSession } from 'next-auth/client';
+import ProgressBar from '../../../common/components/ProgressBar/ProgressBar';
+import ReactStars from 'react-rating-stars-component';
+import Loader from '../../../common/components/Loader/Loader';
 
 export default function Detail(props) {
-  const IdPage = 0;
   const { state, dispatch } = useContext(store);
   const { isOpen } = state;
 
   const [movie, setMovies] = useState(null);
   const router = useRouter();
   const { id } = router.query;
+
+  const [session, loading] = useSession();
 
   useEffect(() => {
     getFilmsDetail(id)
@@ -126,7 +131,63 @@ export default function Detail(props) {
       </div>
       <div className={styles.trailer}>
         <Modal isOpen={isOpen} onClose={() => handleModal()}>
-          estrellas emojis
+          {!session || loading ? (
+            <Loader />
+          ) : (
+            <div className={`container ${styles.profile__container}`}>
+              <div className={styles.user__container}>
+                <img
+                  className={`img-fluid ${styles.user__image}`}
+                  src={`${
+                    session.user.image || 'https://imgur.com/WxZS1Ff.jpg'
+                  }`}
+                  alt={'user'}
+                />
+                <h2 className={`h2 ${styles.username}`}>{session.user.name}</h2>
+              </div>
+
+              <div className={styles.progress__container}>
+                <h4 className={styles.section__title}>You have seen</h4>
+                <div className={styles.progress__container}>
+                  <ProgressBar value={2} max={22} />
+                </div>
+              </div>
+
+              <div className={styles.rating__container}>
+                <h4 className={styles.section__title}>Your stars</h4>
+
+                <div className={styles.rating}>
+                  <ReactStars
+                    count={5}
+                    half={true}
+                    value={4}
+                    edit={false}
+                    size={18}
+                    activeColor="#d1c38b"
+                  />
+                  4 of 5
+                </div>
+              </div>
+              <div className={styles.reaction__container}>
+                <h4 className={styles.section__title}>Your reactions</h4>
+
+                <div className={styles.emojis__container}>
+                  <div className={styles.emoji__container}>
+                    <span className={styles.emoji}>😭</span>
+                    <h6>1</h6>
+                  </div>
+                  <div className={styles.emoji__container}>
+                    <span className={styles.emoji}>😐</span>
+                    <h6>2</h6>
+                  </div>
+                  <div className={styles.emoji__container}>
+                    <span className={styles.emoji}>🤩</span>
+                    <h6>65</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </Modal>
 
         <img
