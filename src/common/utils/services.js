f34efb5bc8e4b7db4a5090a3dbb54b5dff041bc5
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-06-03 22:13:51 -0600

    Displaying user information in profile (films and emojis count, and average star rating)

--- src/common/utils/services.js
+++ src/common/utils/services.js
@@ -1,26 +1,26 @@
-import axios from 'axios'
+import axios from 'axios';
 
-export const getData = async endpoint => {
-  let url = endpoint
-  let res = await axios.get(url)
-  return res.data
-}
+export const getData = async (endpoint) => {
+  let url = endpoint;
+  let res = await axios.get(url);
+  return res.data;
+};
 
 export const getFilms = async () => {
-  let url = 'https://masterghibli.herokuapp.com/films/'
-  let res = await axios.get(url)
-  return res.data
-}
+  let url = 'https://masterghibli.herokuapp.com/films/';
+  let res = await axios.get(url);
+  return res.data;
+};
 
 export const getFilmsDetail = async (id) => {
-  let url = `https://masterghibli.herokuapp.com/films/${id}/`
-  let res = await axios.get(url)
-  return res.data
-}
+  let url = `https://masterghibli.herokuapp.com/films/${id}/`;
+  let res = await axios.get(url);
+  return res.data;
+};
 
 export const getFilmsByUser = async (id) => {
-  let url = 'https://masterghibli.herokuapp.com/ratings/'
-  let res = await axios.get(url)
-  let filterResult = res.data.filter(r => r.user === id)
-  return filterResult
-}
+  let url = 'https://masterghibli.herokuapp.com/ratings/';
+  let res = await axios.get(url);
+  let filterResult = res.data.filter((r) => r.user === id);
+  return filterResult;
+};
