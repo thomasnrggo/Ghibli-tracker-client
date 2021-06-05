91e2f188049e07caac246e051f4a196b9548f5d3
Anthony Gonzalez <thomasnrggo@gmail.com>
2021-06-04 22:24:41 -0500

    new film detail view

--- src/pages/film/[id].jsx
+++ src/pages/film/[id].jsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import Layout from '../../common/components/Layout/Layout';
 import Detail from '../../common/components/Detail/Detail';
 import { useSession } from 'next-auth/client';
+import FilmDetail from '../../common/components/FilmDetail/FilmDetail';
 
 export default function MovieDetail() {
   const router = useRouter();
@@ -11,7 +12,8 @@ export default function MovieDetail() {
 
   return (
     <Layout>
-      <Detail idMovie={id} session={session} loading={loading} />
+      {/* <Detail idMovie={id} session={session} loading={loading} /> */}
+      <FilmDetail moviID={id}/>
     </Layout>
   );
 }
